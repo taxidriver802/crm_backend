358fbfd9f58f22ea6921d93f430c3f1f9e198233
jason cox <JAcox12@icloud.com>
2026-03-19 02:54:53 -0500

    updated docker

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import { Pool } from "pg";
+import { Pool } from 'pg';
 
 const connectionString = process.env.DATABASE_URL;
-if (!connectionString) throw new Error("DATABASE_URL is missing");
+if (!connectionString) throw new Error('DATABASE_URL is missing');
 
 export const pool = new Pool({ connectionString });
